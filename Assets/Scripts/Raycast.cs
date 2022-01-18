6dbc8ffae5fb0b6c2ddb87b8af855d48f7d4967a
Rostislav Goncharov <87071941+RostislavGoncharov@users.noreply.github.com>
2022-01-18 23:00:24 +0200

    Started working on raycast placement

--- Assets/Scripts/Raycast.cs
+++ Assets/Scripts/Raycast.cs
@@ -8,20 +8,47 @@ public class Raycast : MonoBehaviour
     RaycastHit hit;
     [SerializeField] private Transform ground;
     [SerializeField] private GameObject speakerPrefab;
+    [SerializeField] private GameObject speakerTransparent;
 
-    private GameObject suggestedPrefab;
+    private GameObject suggestedPrefab = null;
+
+    bool readyToPlaceSpeaker = false;
+    bool collidingWithSpeaker = false;
 
     void Update()
     {
         if (Physics.Raycast(gameObject.transform.position, gameObject.transform.forward, out hit))
         {
-            //Debug.Log(hit.distance);
 
             if (hit.transform == ground)
             {
-                //Instatntiate the prefab
+                if (suggestedPrefab != null)
+                {
+                    Destroy(suggestedPrefab);
+                    suggestedPrefab = Instantiate(speakerTransparent, hit.point, Quaternion.identity);
+                    readyToPlaceSpeaker = true;
+                }
+                
             }
 
-        } 
+            else
+            {
+                readyToPlaceSpeaker = false;
+                if (suggestedPrefab != null)
+                {
+                    Destroy(suggestedPrefab);
+                }
+            }
+
+        }
+    }
+
+    public void PlaceSpeaker()
+    {
+        if (readyToPlaceSpeaker && !collidingWithSpeaker)
+        {
+            Instantiate(speakerPrefab, suggestedPrefab.transform.position, Quaternion.identity);
+            Debug.Log("speaker placed");
+        }
     }
 }
